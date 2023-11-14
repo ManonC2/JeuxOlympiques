07af904537b9f209c9a6bcac6480b621233b868e
Manon <manon.cvre@gmail.com>
2023-11-14 16:49:56 +0100

    Modification sites et sessions (A poursuivre)

--- src/main/java/org/resources/Controllers/SessionController.java
+++ src/main/java/org/resources/Controllers/SessionController.java
@@ -30,6 +30,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -94,6 +95,71 @@ public class SessionController {
 	@Path("/addSession")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public Response enregistrer(@FormParam("code") String code, @FormParam("date") String date,
+			@FormParam("heureDebut") String heureDebut, @FormParam("heureFin") String heureFin,@FormParam("description") String description, @FormParam("site") int siteId, @FormParam("type") int typeSessionId, @FormParam("epreuve") int epreuveId) {
+
+		Date stringHeureDebut;
+		Date stringHeureFin;
+		Date stringDate;
+		try {
+			stringHeureDebut = (new SimpleDateFormat("yyyy-dd-MM HH:mm:ss")).parse(heureDebut);
+			stringHeureFin = (new SimpleDateFormat("yyyy-dd-MM HH:mm:ss")).parse(heureFin);
+			stringDate = (new SimpleDateFormat("yyyy-dd-MM")).parse(date);
+			Site site = siteRepository.findById(siteId);
+			TypeSession typeSession = typeSessionRepository.findById(typeSessionId);
+			Epreuve epreuve = epreuveRepository.findById(epreuveId);
+			
+			Session session = new Session(code, stringDate, stringHeureDebut, stringHeureFin, description, site, typeSession, epreuve);
+
+			sessionRepository.add(session);
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Response.seeOther(URI.create("http://localhost:8080/JeuxOlympique/web/sessions")).build();
+	}
+	
+	@GET
+	@Path("/updateSession")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String updateSession(@QueryParam("id") String id) {
+		
+		Session session = sessionRepository.findById(Integer.parseInt(id));
+		
+		PebbleEngine engine = new PebbleEngine.Builder().build();
+		PebbleTemplate compiledTemplate = engine.getTemplate("WEB-INF/views/sessions/newSession.html");
+		
+		List<Site> listeSites = siteRepository.findAll();
+		List<TypeSession> listeTypeSession = typeSessionRepository.findAll();
+		List<Epreuve> listeEpreuves = epreuveRepository.findAll();
+		
+
+		Map<String, Object> context = new HashMap<>();
+		context.put("sites", listeSites);
+		context.put("typeSessions", listeTypeSession);
+		context.put("epreuves", listeEpreuves);
+		context.put("session", session);
+		
+		StringWriter writer = new StringWriter();
+
+		try {
+			compiledTemplate.evaluate(writer, context);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+		String output = writer.toString();
+		
+		return output;
+
+	}
+	
+	@POST
+	@Path("/addUpdatedSession")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response addUpdateSession(@FormParam("code") String code, @FormParam("date") String date,
 			@FormParam("heureDebut") String heureDebut, @FormParam("heureFin") String heureFin,@FormParam("description") String description, @FormParam("site") int siteId, @FormParam("type") int typeSessionId, @FormParam("epreuve") int epreuveId) {
 
 		Date stringHeureDebut;
--- src/main/java/org/resources/Controllers/SiteController.java
+++ src/main/java/org/resources/Controllers/SiteController.java
@@ -23,6 +23,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -87,6 +88,53 @@ public class SiteController {
 
 		siteRepository.add(site);
 
+		return Response.seeOther(URI.create("http://localhost:8080/JeuxOlympique/web/sites")).build();
+
+	}
+	
+	@GET
+	@Path("/updateSite")
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String updateSite(@QueryParam("id") String id) {
+		
+		Site site = siteRepository.findById(Integer.parseInt(id));
+		
+		PebbleEngine engine = new PebbleEngine.Builder().build();
+		PebbleTemplate compiledTemplate = engine.getTemplate("WEB-INF/views/sites/newSite.html");
+
+		List<CategorieSite> listeCategoriesSites = categorieSiteRepository.findAll();
+
+		Map<String, Object> context = new HashMap<>();
+		context.put("categoriesSites", listeCategoriesSites);
+		context.put("site", site);
+
+		StringWriter writer = new StringWriter();
+
+		try {
+			compiledTemplate.evaluate(writer, context);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String output = writer.toString();
+
+		return output;
+
+	}
+	
+	@POST
+	@Path("/addUpdatedSite")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Response addUpdateSite(@FormParam("nom") String nom, @FormParam("ville") String ville,
+			@FormParam("categorieSite") int categorieSiteId, @QueryParam("id") String id) {
+
+		CategorieSite categorieSite = categorieSiteRepository.findById(categorieSiteId);
+
+		Site site = new Site(Integer.parseInt(id), nom , ville, categorieSite);
+
+		siteRepository.update(site);
+		
 		return Response.seeOther(URI.create("http://localhost:8080/JeuxOlympique/web/sites")).build();
 
 	}
